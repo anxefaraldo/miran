5ed61f717c391675adcd09d53849410be7873e9b
Ángel Faraldo <angelfaraldo@gmail.com>
2018-01-03 11:00:41 +0100

    fixing eval

--- scripts/eval-key.py
+++ scripts/eval-key.py
@@ -11,6 +11,9 @@ ones defined in miran.defs.ANNOTATION_FILE_EXTENSIONS.
 (Last update: December 2017)
 
 """
+
+from __future__ import print_function
+
 if __name__ == "__main__":
 
     import os.path
@@ -24,7 +27,7 @@ if __name__ == "__main__":
     parser = ArgumentParser(description="Evaluation of key estimation algorithms.")
     parser.add_argument("references", help="dir with reference annotations.")
     parser.add_argument("estimations", help="dir with estimated labels")
-    parser.add_argument("-v", "--vocabulary", default='majmin-ambiguous',
+    parser.add_argument("-v", "--vocabulary", default='majmin-single',
                         help="evaluation method (majmin-single, majmin-ambiguous, other-single, other-ambiguous, "
                              "nokey-single, nokey-ambiguous, all-single, all-ambiguous)")
     parser.add_argument("-s", "--save_to_file", help="save the evaluation results to an excel spreadsheet")
@@ -35,7 +38,7 @@ if __name__ == "__main__":
         raise parser.error("Warning: '{}' or '{}' not a directory.".format(args.references, args.estimations))
 
     else:
-        print("\nEvaluating with {}\n".format(args.vocabulary))
+        print("Evaluating with {}\n".format(args.vocabulary))
 
         mtx_key = np.array(np.zeros(37 * 37).reshape(37, 37), dtype=int)
         mtx_error = np.array(np.zeros(37 * 4).reshape(37, 4), dtype=int)
@@ -43,53 +46,62 @@ if __name__ == "__main__":
         mirex = []
         errors = []
         results = {}
-        file_count = 0
-        unknown_files = 0
+        files_estimated = 0
+        files_evaluated = 0
+        unknown_estimations = 0
+        unknown_references = 0
         raw_reference = ''
         true_tonic_mode = np.array([0, 0])
         false_tonic_mode = np.array([0, 0])
         estimations = os.listdir(args.estimations)
 
         for each_file in estimations:
+
             ests = []
             refs = []
+
             if any(ext == os.path.splitext(each_file)[-1] for ext in ANNOTATION_FILE_EXTENSIONS):
+
+                print('Evaluating "{}" ... '.format(each_file), end='')
 
                 with open(os.path.join(args.estimations, each_file), 'r') as analysis:
                     raw_estimation = analysis.readline()
-                    # if 'majmin' in args.vocabulary:
-                    #     if 'other' in raw_estimation:
-                    #         print("estimation has 'other'", analysis)
-                    #         file_count += 1
-                    #         unknown_files += 1
-                    #         continue
-                    #     elif 'X' in raw_estimation:
-                    #         print("estimation has 'no-key' in", analysis)
-                    #         file_count += 1
-                    #         unknown_files += 1
-                    #         continue
-                    #
-                    # elif 'other' in args.vocabulary:
-                    #     if 'X' in raw_estimation:
-                    #         print("estimation has 'no-key' in", raw_estimation)
-                    #         file_count += 1
-                    #         unknown_files += 1
-                    #         continue
-                    #
-                    # elif 'nokey' in args.vocabulary:
-                    #     if 'other' in raw_estimation:
-                    #         print("estimation has 'other' in", raw_estimation)
-                    #         file_count += 1
-                    #         unknown_files += 1
-                    #         continue
 
-                    # TODO: poner la cuenta de archivos al final del ifs..
-                    # Con 'other' en el argumento funciona diferente!
-                    # Ahora NO>... ejem....
 
+
+
+
+
+
+                    if "nknown" in raw_estimation:
+                        print("estimation is 'unknown' ", end='')
+                        unknown_estimations += 1
+                        #continue
+
+                    if 'majmin' in args.vocabulary:
+                        if 'other' in raw_estimation:
+                            print("estimation has 'other' ", end='')
+                            unknown_estimations += 1
+                            #continue
+
+                        elif 'X' in raw_estimation:
+                            print("estimation has 'no-key' ", end='')
+                            unknown_estimations += 1
+                            #continue
+
+                    elif 'other' in args.vocabulary:
+                        if 'X' in raw_estimation:
+                            print("estimation has 'no-key' ", end='')
+                            unknown_estimations += 1
+                            #continue
+
+                    elif 'nokey' in args.vocabulary:
+                        if 'other' in raw_estimation:
+                            print("estimation has 'no-key' ", end='')
+                            unknown_estimations += 1
+                            #continue
 
                     raw_estimations = raw_estimation.split(' | ')
-                    print raw_estimation
                     raw_estimation = raw_estimation.replace('\t', ' ')
                     raw_estimation = raw_estimation.replace('\n', '')
 
@@ -100,51 +112,58 @@ if __name__ == "__main__":
                     try:
                         with open(os.path.join(args.references, os.path.splitext(each_file)[0] + ext), 'r') as reference:
                             raw_reference = reference.readline()
+
+                            if "nknown" in raw_reference: # TODO: CHECK THAT THIS WILL COVER ALL SCENARIOS
+                                print("reference is 'unknown'", end='')
+                                files_evaluated += 1
+                                unknown_estimations += 1
+                                continue
+
                             if 'majmin' in args.vocabulary:
                                 if 'other' in raw_reference:
-                                    print("'other' in", reference)
-                                    file_count += 1
-                                    unknown_files += 1
+                                    print("reference has 'other' ", end='')
+                                    files_evaluated += 1
+                                    unknown_estimations += 1
                                     continue
+
                                 elif 'X' in raw_reference:
-                                    print("'no-key' in", reference)
-                                    file_count += 1
-                                    unknown_files += 1
+                                    print("reference has 'no-key' ", end='')
+                                    files_evaluated += 1
+                                    unknown_estimations += 1
                                     continue
+
                             elif 'other' in args.vocabulary:
                                 if 'X' in raw_reference:
-                                    print("'no-key' in", reference)
-                                    file_count += 1
-                                    unknown_files += 1
+                                    print("reference has 'no-key' ", end='')
+                                    files_evaluated += 1
+                                    unknown_estimations += 1
                                     continue
+
                             elif 'nokey' in args.vocabulary:
                                 if 'other' in raw_reference:
-                                    print("'other' in", reference)
-                                    file_count += 1
-                                    unknown_files += 1
+                                    print("reference has 'other' ", end='')
+                                    files_evaluated += 1
+                                    unknown_estimations += 1
                                     continue
 
                             raw_references = raw_reference.split(' | ')
                             raw_reference = raw_reference.replace('\t', ' ')
                             raw_reference = raw_reference.replace('\n', '')
+
                             for item in raw_references:
-                                item = split_key_str(item)
-                                if -2 in item:
-                                    file_count += 1
-                                    unknown_files += 1
-                                    continue
+                                refs.append(split_key_str(item))
 
-                                refs.append(item)
-
-                        break
+                        # break
 
                     except IOError:
                         continue
 
-
                 if not ests or not refs:
-                    print("{} - Didn't find a suitable reference".format(each_file))
+                    print("Didn't find a suitable reference.")
                     continue
+
+                else:
+                    print("OK!")
 
                 # here I define the various evaluation methods...
                 if 'ambiguous' in args.vocabulary:
@@ -159,6 +178,7 @@ if __name__ == "__main__":
 
                     for estimated_key in ests:
                         for reference_key in refs:
+                            print(estimated_key, reference_key)
                             score_mirex = key_eval_mirex(estimated_key, reference_key)
                             premirex.append(score_mirex)
 
@@ -180,20 +200,8 @@ if __name__ == "__main__":
                 type_error = key_eval_relative_errors(estimated_key, reference_key)
                 errors.append(type_error[0])
 
-                # # PRE BIMODAL
-                # results[each_file] = pd.Series([pc_to_chroma(reference_key[0]), id_to_mode(reference_key[1]),
-                #                                 pc_to_chroma(estimated_key[0]), id_to_mode(estimated_key[1]),
-                #                                 type_error[1], score_mirex],
-                #                                 index=['ref_tonic', 'ref_mode',
-                #                                        'est_tonic', 'est_mode',
-                #                                        'rel_error', 'mirex'])
-
-                # POST BIMODAL
-                results[each_file] = pd.Series([raw_reference,
-                                                raw_estimation,
-                                                type_error[1], score_mirex],
+                results[each_file] = pd.Series([raw_reference, raw_estimation, type_error[1], score_mirex],
                                                 index=['reference', 'estimation', 'rel_error', 'mirex'])
-
 
                 if estimated_key[1] is None:
                     estimated_key[1] = 0
@@ -204,7 +212,7 @@ if __name__ == "__main__":
                 row = estimated_key[0] + (estimated_key[1] * 12)
                 mtx_key[row, col] += 1
 
-                file_count += 1
+                files_evaluated += 1
 
         # GENERAL EVALUATION
         # ==================
@@ -218,8 +226,8 @@ if __name__ == "__main__":
                            mirex.count(0.0),
                            np.sum(mirex)], float(len(errors)))
 
-        true_tonic_mode = np.divide(true_tonic_mode, float(file_count))
-        false_tonic_mode = np.divide(false_tonic_mode, float(file_count))
+        true_tonic_mode = np.divide(true_tonic_mode, float(files_evaluated))
+        false_tonic_mode = np.divide(false_tonic_mode, float(files_evaluated))
 
         # PRINT RESULTS TO CONSOLE
         # ========================
@@ -227,28 +235,18 @@ if __name__ == "__main__":
         pd.set_option('max_columns', 100)
         pd.set_option('expand_frame_repr', False)
 
-        # PRE-BIMODAL
-        #results = pd.DataFrame(results, index=['ref_tonic', 'ref_mode', 'est_tonic', 'est_mode', 'rel_error', 'mirex']).T
-
-        # POST BIMODAL
         results = pd.DataFrame(results, index=['reference', 'estimation', 'rel_error', 'mirex']).T
+        print('\n')
         print(results)
 
         print('\nCONFUSION MATRIX:')
         print('reference keys (cols) vs. estimations (rows)\n')
         mtx_key = pd.DataFrame(mtx_key.T, index=KEY_LABELS, columns=KEY_LABELS)
-        #if args.eval_method == 'mirex':
-        #    mtx_key = mtx_key[:24]
-        #    mtx_key = mtx_key.filter(KEY_LABELS[:24])
         print(mtx_key)
 
         print("\nRELATIVE ERROR MATRIX:\n")
         mtx_error = pd.DataFrame(mtx_error.T, index=('I', 'i', '1?', 'X'), columns=DEGREE_LABELS)
         print(mtx_error)
-        #if args.vocabulary == 'majmin':
-         #   mtx_error = mtx_error[:2]
-          #  mtx_error = mtx_error.filter(DEGREE_LABELS[:24])
-
 
         print("\nTONIC MODE BASELINE:")
         tonic_mode = pd.DataFrame([true_tonic_mode, false_tonic_mode], index=['true', 'false'], columns=['tonic', 'mode'])
@@ -258,7 +256,9 @@ if __name__ == "__main__":
         mirex = pd.DataFrame(mirex, index=['correct', 'fifth', 'relative', 'parallel', 'other', 'weighted'], columns=['%'])
         print(mirex)
 
-        print("\n{} files processed, {} evaluated, {} unknown.".format(file_count, file_count-unknown_files, unknown_files))
+        print("\n{} estimation files, {} valid evaluation files.".format(files_estimated,  files_evaluated))
+        print("{} discarded unknown estimations, {} discarded unknown evaluations".format(unknown_estimations, unknown_references))
+        print("{} total evaluated files".format(files_evaluated))
 
         # WRITE RESULTS TO FILE
         # =====================
